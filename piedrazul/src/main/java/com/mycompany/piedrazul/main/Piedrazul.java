32fde8bfd894cdf4d0ec97be3f973fa00c645b09
rhony-martinez <rhony@unicauca.edu.co>
2026-02-17 12:31:00 -0500

    Estructura de proyecto

--- piedrazul/src/main/java/com/mycompany/piedrazul/main/Piedrazul.java
+++ piedrazul/src/main/java/com/mycompany/piedrazul/main/Piedrazul.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.piedrazul;
+package com.mycompany.piedrazul.main;
 
 /**
  *
