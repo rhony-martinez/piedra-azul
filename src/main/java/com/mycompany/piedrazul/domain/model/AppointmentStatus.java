605e37838bfc019f4f1758a8dd21a7a0c8550874
RhonyMartinez <mbrhony4806@gmail.com>
2026-04-18 12:14:07 -0500

    restauracion

--- src/main/java/com/mycompany/piedrazul/domain/model/AppointmentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.piedrazul.domain.model;
-
-public enum AppointmentStatus {
-    PROGRAMADA,      // Agendada
-    CANCELADA,      // Confirmada
-    ATENDIDA,    // En curso
-    NO_ASISTIDA,      // Completada
-}
